85c2af8f2491626cf9de9c1c92e3a4d65ef0893b
juan pablo troccay <jptroccay@gmail.com>
2024-02-13 09:20:41 -0300

    production

--- client/script.js
+++ client/script.js
@@ -76,7 +76,7 @@ const handleSubmit = async (e) => {
   loader(messageDiv);
 
   // fetch data from server -> bot's response
-  const response = await fetch("http://localhost:5001", {
+  const response = await fetch("https://cohere-ai.onrender.com", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
